e354bb056985c8f57ac5fafaf31f879262e64364
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-12-17 18:43:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env.read_env()
 
 TG_BOT_TOKEN = env.str("TG_BOT_TOKEN")
 DEVMAN_TOKEN = env.str("DEVMAN_TOKEN")
-TG_CHAT_ID = env.str("TG_CHAT_ID")
+TG_CHAT_ID = env.int("TG_CHAT_ID")
 
 LOGGING_CONFIG = {
     "version": 1,

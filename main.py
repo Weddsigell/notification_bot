f9c4b77de8a947b27b7b1b3ea517f8dcd5d2888d
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-12-17 23:22:54 +0500

    create check_dvmn_status

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import logging
 import logging.config
-import aiohttp
-import asyncio
 
-import requests
+import aiohttp
 import telegram
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
@@ -26,38 +24,77 @@ async def check_dvmn_status(context, chat_id):
     if not dvmn_token or not isinstance(dvmn_token, str):
         logger.critical("DEVMAN_TOKEN пуст или неверного типа")
         raise ValueError("DEVMAN_TOKEN пуст или неверного типа")
-    
-    logger.debug("DEVMAN_TOKEN получен, его длинна %s символов", len(dvmn_token))
+
+    logger.debug(
+        "DEVMAN_TOKEN получен, его длинна %s символов", len(dvmn_token)
+    )
 
     url = "https://dvmn.org/api/long_polling/"
-    headers = {"Authorization": f"Token {config.DEVMAN_TOKEN}"}
+    headers = {"Authorization": f"Token {dvmn_token}"}
     params = {}
     timeout = aiohttp.ClientTimeout(total=120)
 
-    while True:
-        response = requests.get(
-            url=url,
-            headers=headers,
-            params=params,
-            timeout=timeout,
+    async with aiohttp.ClientSession(
+        headers=headers, timeout=timeout
+    ) as session:
+        logger.debug(
+            "Запуск сессии с параметрами headers: %s, timeout: %s",
+            headers,
+            timeout,
         )
-        params.clear()
-        response.raise_for_status()
 
-        response = response.json()
-        print(response["request_query"])
-        status = response["status"]
-        logger.info("сервер вернул статус %s", status)
-
-        if status == "found":
-            await context.bot.send_message(
-                chat_id=chat_id, text="Работа проверена!"
+        while True:
+            logger.debug(
+                "Get запрос с параметрами url: %s, params: %s", url, params
             )
-            logger.info("Сервер вернул нужные данные")
-        else:
-            timestamp = int(response["timestamp_to_request"])
-            params["timestamp"] = timestamp
-            logger.info("Повторный запрос с timestamp: %s", timestamp)
+            async with session.get(url, params=params) as response:
+                status = response.status
+                raw_text = await response.text()
+
+                if not status == 200:
+                    logger.error(
+                        "Получен ответ. Код: %s\nТекст: %s",
+                        status,
+                        raw_text,
+                    )
+                    continue
+
+                try:
+                    response_json = await response.json()
+                except Exception as e:
+                    logger.error("Не удалось распарсить ответ в json: %s", e)
+                    continue
+                finally:
+                    logger.debug(
+                        "Получен ответ. Код: %s\nТекст: %s",
+                        status,
+                        raw_text,
+                    )
+
+            dvmn_status = response_json["status"]
+
+            if dvmn_status == "found":
+                new_attempts = response_json.get("new_attempts")
+
+                for attempt in new_attempts:
+                    name_lesson = attempt["lesson_title"]
+                    text = f"Работа {name_lesson} проверена!\n"
+
+                    if attempt["is_negative"]:
+                        text += "К сожалению есть ошибки, нужно исправить!"
+                    else:
+                        text += "Ты большой молодец, ошибок нет!"
+
+                    context.bot.send_message(chat_id=chat_id, text=text)
+
+            timestamp_to_request = int(
+                response_json.get("timestamp_to_request")
+            )
+
+            if timestamp_to_request is not None:
+                params["timestamp"] = timestamp_to_request
+            else:
+                params.clear()
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -73,7 +110,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if not user_id or not isinstance(user_id, int):
         logger.critical("TG_CHAT_ID пуст или неверного типа")
         raise ValueError("TG_CHAT_ID пуст или неверного типа")
-    
+
     logger.debug("TG_CHAT_ID получен - %s", user_id)
 
     if not chat_id == user_id:
